fed2a9f56bb624e8c561160e0fe6bb52b17d00bb
Dhiyanamoorthy <dhiyanamoorthy@gamil.com>
2026-08-13 15:46:15 +0530

    seoond commet

--- swap.java
+++ swap.java
@@ -1,8 +1,13 @@
-public class swap {
-    public static void main(String[] args) {
+import java.util.*;
 
-        int a = 10;
-        int b = 20;
+public class swap {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a value: ");
+        int a = sc.nextInt();
+        System.out.println("Enter b value: ");
+        int b = sc.nextInt();
 
         System.out.println("Before swapping:");
         System.out.println("a = " + a);
